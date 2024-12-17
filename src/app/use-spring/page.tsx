e207042d827e441bd9160fb5873b278463540de9
[AI10] 김민규 <kmg09190@naver.com>
2024-12-17 18:03:36 +0900

    📰 use-spring 실습 수정

--- src/app/use-spring/page.tsx
+++ src/app/use-spring/page.tsx
@@ -7,7 +7,7 @@ function AnimatedNumberFramerMotion({ value }: { value: number }) {
   // Motion Value 생성
   const motionValue = useMotionValue(0);
 
-  // useSpring을 사용해 부드러운 애니메이션 적용
+  // useSpring을 사용해 애니메이션 적용
   const springValue = useSpring(motionValue, { stiffness: 100, damping: 20 });
 
   // useTransform을 사용해 소수점을 정수로 변환
@@ -31,15 +31,24 @@ function App() {
   const [num, setNum] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
-      const newNum = (Math.random() * 100).toFixed();
+      const newNum = (Math.random() * 49).toFixed();
       setNum(parseInt(newNum));
     }, 2000);
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: 2 }}>
-      <AnimatedNumberFramerMotion value={num} />
+    <div
+      style={{
+        maxWidth: "1280px",
+        margin: "0 auto",
+        padding: "2rem",
+        textAlign: "center",
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column", gap: 2 }}>
+        <AnimatedNumberFramerMotion value={num} />
+      </div>
     </div>
   );
 }
